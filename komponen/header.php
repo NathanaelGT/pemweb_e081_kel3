850079da97365a0807bbcb5cb42b1c3baee87102
NathanaelGT <gordontanu2004@gmail.com>
2024-05-28 16:28:37 +0700

    Memperbaiki bug url

--- komponen/header.php
+++ komponen/header.php
@@ -1,24 +1,24 @@
 <header>
     <nav>
         <a href="./" class="home-link">
-            <img src="<?= $basePath ?? './' ?>assets/logo.svg" alt="Logo" class="logo"/>
+            <img src="<?= $basePath ?>assets/logo.svg" alt="Logo" class="logo"/>
             <span>Jelajah<br/>Pustaka</span>
         </a>
 
         <div class="spacer"></div>
 
         <ul>
-            <li><a href="./">Beranda</a></li>
+            <li><a href="<?= $basePath ?>">Beranda</a></li>
 
             <?php if ($pengguna = pengguna()): ?>
                 <?php if ($pengguna->getAdmin()): ?>
-                    <li><a href="./admin/buku.php">Daftar Buku</a></li>
+                    <li><a href="<?= $basePath ?>admin/buku.php">Daftar Buku</a></li>
                 <?php endif ?>
 
-                <li><a href="./keluar.php">Keluar</a></li>
+                <li><a href="<?= $basePath ?>keluar.php">Keluar</a></li>
             <?php else: ?>
-                <li><a href="./masuk.php">Masuk</a></li>
-                <li><a href="./daftar.php">Daftar</a></li>
+                <li><a href="<?= $basePath ?>masuk.php">Masuk</a></li>
+                <li><a href="<?= $basePath ?>daftar.php">Daftar</a></li>
             <?php endif ?>
         </ul>
     </nav>
