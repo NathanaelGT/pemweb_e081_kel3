1c2478c97b9a95866d10931ae3ace800baa5af7a
indrianii <indrianiana28@gmail.com>
2024-06-02 01:01:22 +0700

    memperbaiki bug di laman review

--- submit_review.php
+++ submit_review.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($pengguna) {
         $id_buku = $_POST['id_buku'];
         $ulasan = $_POST['ulasan'];
-        $penilaian = $_POST['penilaian'];
+        $penilaian = isset($_POST['penilaian']) ? $_POST['penilaian'] : null;
 
         // Tulis dan simpan ulasan
         $review = new Ulasan([
@@ -17,13 +17,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ]);
         $review->simpan();
 
-        // Tulis dan menyimpan rating/penilaian
-        $rating = new Penilaian([
-            'id_buku' => $id_buku,
-            'id_pengguna' => $pengguna->getId(),
-            'penilaian' => $penilaian
-        ]);
-        $rating->simpan();
+        // Tulis dan menyimpan rating/penilaian jika nilainya tidak null
+        if ($penilaian !== null) {
+            $rating = new Penilaian([
+                'id_buku' => $id_buku,
+                'id_pengguna' => $pengguna->getId(),
+                'penilaian' => $penilaian
+            ]);
+            $rating->simpan();
+        }
         
         // mengarahkan ke laman ulasan lagi
         header('Location: review.php?id=' . $id_buku);
