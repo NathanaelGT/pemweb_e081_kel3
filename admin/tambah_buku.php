0fee932b0c5863e4e80f5b10f527825445706488
NathanaelGT <gordontanu2004@gmail.com>
2024-06-16 11:57:28 +0700

    Membuat variabel untuk menyimpan config locale dari AirDatepicker

--- admin/tambah_buku.php
+++ admin/tambah_buku.php
@@ -88,20 +88,7 @@ HTML ?>
 
 <script>
     document.addEventListener('DOMContentLoaded', () => {
-        new AirDatepicker('input[name="terbit"]', {
-            locale: {
-                days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
-                daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
-                daysMin: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
-                months: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
-                monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des'],
-                today: 'Hari ini',
-                clear: 'Hapus',
-                dateFormat: 'dd/MM/yyyy',
-                timeFormat: 'hh:mm aa',
-                firstDay: 1
-            }
-        })
+        new AirDatepicker('input[name="terbit"]', { locale: airDatepickerLocale })
     })
 </script>
 
--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,16 @@
+const airDatepickerLocale = {
+    days: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+    daysShort: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+    daysMin: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+    months: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agt', 'Sep', 'Okt', 'Nov', 'Des'],
+    today: 'Hari ini',
+    clear: 'Hapus',
+    dateFormat: 'dd/MM/yyyy',
+    timeFormat: 'hh:mm aa',
+    firstDay: 1
+}
+
 const xIcon = '<svg fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/></svg>'
 
 document.querySelectorAll('.info').forEach(info => {
